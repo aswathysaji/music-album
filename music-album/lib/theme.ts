c3ba7aabfbb23dd008370a35f4847ed57b30d5b7
u231870 <aswathy@strollby.com>
2025-03-06 13:16:17 +0530

    chore: initial project setup

---
+++ music-album/lib/theme.ts
@@ -0,0 +1,7 @@
+import createTheme from "@mui/material/styles/createTheme";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: ["Commissioner, sans-serif"].join(","),
+  },
+});
