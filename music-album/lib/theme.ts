039eb45d908e67f927bb9a822acf8af7342e6603
Aswathy Saji <69248795+aswathysaji@users.noreply.github.com>
2025-03-06 13:22:00 +0530

    Delete music-album directory

--- music-album/lib/theme.ts
+++
@@ -1,7 +0,0 @@
-import createTheme from "@mui/material/styles/createTheme";
-
-export const theme = createTheme({
-  typography: {
-    fontFamily: ["Commissioner, sans-serif"].join(","),
-  },
-});
