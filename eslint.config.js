45f568ec911e88953b07302e79305a538c4704f5
u231870 <aswathy@strollby.com>
2025-03-07 10:30:15 +0530

    chore: add no console lint

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,14 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "no-console": "off",
+        "no-restricted-syntax": [
+            "error",
+            {
+                "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+                "message": "Unexpected property on console object was called"
+            }
+        ]
     },
   },
 )
